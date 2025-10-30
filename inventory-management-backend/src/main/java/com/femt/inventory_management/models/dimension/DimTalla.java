ecbc10cbfa1fc0b9ed8717578aa9a4c337ac844a
MenesesTech <menesesfrey@gmail.com>
2025-10-30 12:40:39 -0500

    Se elimina anotaciones innecesarias, de las clases hijas

--- inventory-management-backend/src/main/java/com/femt/inventory_management/models/dimension/DimTalla.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/models/dimension/DimTalla.java
@@ -3,8 +3,6 @@ package com.femt.inventory_management.models.dimension;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
