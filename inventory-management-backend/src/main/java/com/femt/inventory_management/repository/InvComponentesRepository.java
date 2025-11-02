f95ba2c2d258d4609e84755dc9cad17e247c3f6b
MenesesTech <menesesfrey@gmail.com>
2025-11-02 13:23:43 -0500

    Se agrego metodos para busquedas por filtros

--- inventory-management-backend/src/main/java/com/femt/inventory_management/repository/InvComponentesRepository.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/repository/InvComponentesRepository.java
@@ -4,6 +4,18 @@ import com.femt.inventory_management.models.inventario.InventarioComponentes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InvComponentesRepository extends JpaRepository<InventarioComponentes, Integer> {
+    /**
+     * Buscar un SKU especifico en el inventario
+     */
+    Optional<InventarioComponentes> findByCategoria_IdAndModelo_IdAndTipoComponente_IdAndTalla_IdAndColor_Id(
+            Integer idCategoria,
+            Integer idModelo,
+            Integer idTipoComponente,
+            Integer idTalla,
+            Integer idColor
+    );
 }
--- inventory-management-backend/src/main/java/com/femt/inventory_management/repository/InvProductoTerminadoRepository.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/repository/InvProductoTerminadoRepository.java
@@ -4,6 +4,21 @@ import com.femt.inventory_management.models.inventario.InventarioProductoTermina
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+/**
+ * Busca un SKU específico en inventario
+ */
 @Repository
 public interface InvProductoTerminadoRepository extends JpaRepository<InventarioProductoTerminado, Integer> {
+    /**
+     * Busca un SKU específico en inventario
+     */
+    Optional<InventarioProductoTerminado> findByCategoria_IdAndModelo_IdAndTalla_IdAndColorPlanta_IdAndColorTira_Id(
+            Integer idCategoria,
+            Integer idModelo,
+            Integer idTalla,
+            Integer idColorPlanta,
+            Integer idColorTira
+    );
 }
