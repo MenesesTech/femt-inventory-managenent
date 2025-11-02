a8b8015f79964ba0fa71eae76b1e7e68376363c2
MenesesTech <menesesfrey@gmail.com>
2025-11-02 12:48:31 -0500

    Se añadio metodo para buscar series mediantes filtros como modelo y categoria

--- inventory-management-backend/src/main/java/com/femt/inventory_management/repository/LogSerieRepository.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/repository/LogSerieRepository.java
@@ -4,6 +4,15 @@ import com.femt.inventory_management.models.serie.LogSerie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LogSerieRepository extends JpaRepository<LogSerie, Integer> {
+    /**
+     * Busca los colores de una serie
+     * basada en Categoría, Modelo y Serie.
+     */
+    List<LogSerie> findByDimCategoria_IdAndDimModelo_IdAndDimSerie_Id(
+            Integer idCategoria, Integer idModelo, Integer idSerie
+    );
 }
