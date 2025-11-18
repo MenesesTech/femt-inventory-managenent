1e20dc13ac6de60041657d04a99bf2c02ea294a6
MenesesTech <menesesfrey@gmail.com>
2025-11-18 06:12:55 -0500

    Se agrego consultas especificas para el manejo de servicios de dimensones

--- inventory-management-backend/src/main/java/com/femt/inventory_management/repository/DimCategoriaRepository.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/repository/DimCategoriaRepository.java
@@ -4,6 +4,10 @@ import com.femt.inventory_management.models.dimension.DimCategoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DimCategoriaRepository extends JpaRepository<DimCategoria, Integer> {
+    boolean existsByNombre(String nombre);
+    List<DimCategoria> findAllByOrderByIdAsc();
 }
--- inventory-management-backend/src/main/java/com/femt/inventory_management/repository/DimTallaRepository.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/repository/DimTallaRepository.java
@@ -4,6 +4,10 @@ import com.femt.inventory_management.models.dimension.DimTalla;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DimTallaRepository extends JpaRepository<DimTalla, Integer> {
+    boolean existsByNombre(String nombre);
+    List<DimTalla> findAllByOrderByIdAsc();
 }
