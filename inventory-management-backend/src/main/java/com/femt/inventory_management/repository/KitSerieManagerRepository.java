e433f23727894268ddd9030c2278867700c1d66f
MenesesTech <menesesfrey@gmail.com>
2025-11-24 20:42:48 -0500

    Se corrigio la entidad asociada al repositorio

--- inventory-management-backend/src/main/java/com/femt/inventory_management/repository/KitSerieManagerRepository.java
+++ inventory-management-backend/src/main/java/com/femt/inventory_management/repository/KitSerieManagerRepository.java
@@ -1,6 +1,7 @@
 package com.femt.inventory_management.repository;
 
 import com.femt.inventory_management.models.kit.KitSerieColor;
+import com.femt.inventory_management.models.kit.KitSerieManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface KitSerieManagerRepository extends JpaRepository<KitSerieColor, Integer> {
+public interface KitSerieManagerRepository extends JpaRepository<KitSerieManager, Integer> {
 
 }
